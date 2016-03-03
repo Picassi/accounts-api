f12be65ea423598022f8178b665cb1be726f536c
mdsomerfield <somerfield.md@gmail.com>
2016-03-03 21:11:57 +0000

    Added test projects for API and core libraries

---
+++ src/Picassi.Api.Accounts/Picassi.Core.Accounts.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picassi.Core.Accounts.Tests
+{
+    public class Class1
+    {
+    }
+}
