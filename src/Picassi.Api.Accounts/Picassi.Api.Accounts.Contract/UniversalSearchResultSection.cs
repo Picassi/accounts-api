8e2085ba131ec8a66f2fe3595ca35b40be9843e6
Mark Somerfield <somerfield.md@gmail.com>
2017-07-19 12:38:23 +0100

    Added universal search API

---
+++ src/Picassi.Api.Accounts/Picassi.Api.Accounts.Contract/UniversalSearchResultSection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Picassi.Api.Accounts.Contract
+{
+    public class UniversalSearchResultSection
+    {
+        public string Title { get; set; }
+        public IEnumerable<UniversalSearchResult> Results { get; set; }
+    }
+
+    public class UniversalSearchResult
+    {
+        public string ResultText { get; set; }
+
+        public IEnumerable<UniversalSearchResultAction> Actions { get; set; }
+    }
+
+    public class UniversalSearchResultAction
+    {
+        public string ActionText { get; set; }
+
+        public ActionType ActionType { get; set; }
+
+        public string ActionKey { get; set; }
+    }
+
+    public enum ActionType
+    {
+        EditTransaction
+    }
+}
---
+++ src/Picassi.Api.Accounts/Picassi.Api.Accounts/Controllers/SearchController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Picassi.Api.Accounts.Contract;
+using Picassi.Core.Accounts.DAL.Services;
+using Picassi.Core.Accounts.Models.Transactions;
+using Picassi.Utils.Api.Attributes;
+
+namespace Picassi.Api.Accounts.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [PicassiApiAuthorise]
+    public class SearchController : ApiController
+    {
+        private readonly ITransactionsDataService _transactionsDataService;
+
+        public SearchController(ITransactionsDataService transactionsDataService)
+        {
+            _transactionsDataService = transactionsDataService;
+        }
+
+        [HttpGet]
+        [Route("search")]
+        public IEnumerable<UniversalSearchResultSection> GetResults([FromUri] string searchText)
+        {
+            var transactions = _transactionsDataService.Query(searchText);
+
+            return new List<UniversalSearchResultSection>
+            {
+                new UniversalSearchResultSection
+                {
+                    Title = "Transactions",
+                    Results = transactions.Select(MapToTransactionResult)
+                }
+            };
+
+        }
+
+        private static UniversalSearchResult MapToTransactionResult(TransactionModel transaction)
+        {
+            return new UniversalSearchResult
+            {
+                ResultText = transaction.Description,
+                Actions = new List<UniversalSearchResultAction>
+                {
+                    new UniversalSearchResultAction
+                    {
+                        ActionKey = transaction.Id.ToString(),
+                        ActionText = "Edit",
+                        ActionType = ActionType.EditTransaction
+                    }
+                }
+            };
+        }
+    }
+}
