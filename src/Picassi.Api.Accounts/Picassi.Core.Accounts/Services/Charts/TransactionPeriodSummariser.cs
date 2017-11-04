edd2872527b1940e68242acc621bd75afc485fcd
Mark Somerfield <somerfield.md@gmail.com>
2017-11-04 22:25:43 +0000

    Attempted fix for bad data on category graphs

--- src/Picassi.Api.Accounts/Picassi.Core.Accounts/Services/Charts/TransactionPeriodSummariser.cs
+++ src/Picassi.Api.Accounts/Picassi.Core.Accounts/Services/Charts/TransactionPeriodSummariser.cs
@@ -89,6 +89,7 @@ namespace Picassi.Core.Accounts.Services.Charts
                     }
 
                     currentSummary.EndBalance = transaction.Balance;
+                    if (transaction.Date < dt) continue;
                     if (transaction.Amount > 0) currentSummary.TotalIncome += transaction.Amount;
                     if (transaction.Amount < 0) currentSummary.TotalSpending -= transaction.Amount;
                 }
